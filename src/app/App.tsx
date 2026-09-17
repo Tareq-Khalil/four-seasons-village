c74565ac9ddbcbd09e0c4531b21fb9bff4116a19
Mazen Khalil <mazenahmed1721@gmail.com>
2026-09-17 22:38:29 +0300

    updates App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AppShell } from "../components/layout/AppShell"
 import { HomePage } from "../pages/HomePage";
+import { VillagePage } from "../pages/VillagePage";
 import { LoginPage } from "../pages/LoginPage";
 import { AdminPage } from "../pages/AdminPage";
 
@@ -9,6 +10,7 @@ export function App() {
     <Routes>
       <Route path="/" element={<HomePage />}/>
       <Route element={<AppShell/>}>
+        <Route path="/village" element={<VillagePage />} />
         <Route path="/admin" element={<AdminPage/>} />
       </Route>
       <Route path="/login" element={<LoginPage/>}/>
