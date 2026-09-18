b6fa6708fc5aae9fd194592e7a78fc01c04581e8
Tarek Khalil <tareqkhalil1415@gmail.com>
2026-09-18 16:01:21 +0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,7 @@ import { JournalPage } from "../pages/JournalPage";
 import { InventoryPage } from "../pages/InventoryPage";
 import { ProfilePage } from "../pages/ProfilePage";
 import { MapPage } from "../pages/MapPage";
+import { QuestsPage} from "../pages/QuestsPage";
 import { LoginPage } from "../pages/LoginPage";
 import { AdminPage } from "../pages/AdminPage";
 import { useGame } from "./GameProvider";
@@ -41,54 +42,13 @@ export function App() {
       <Route path="/" element={<HomePage />}/>
       <Route path="/login" element={<LoginPage/>}/>
       <Route element={<AppShell />}>
-        <Route 
-          path="/village" 
-          element={
-            <ProtectedRoute>
-              <VillagePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/map" 
-          element={
-            <ProtectedRoute>
-              <MapPage/>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/inventory" 
-          element={
-            <ProtectedRoute>
-              <InventoryPage/>
-            </ProtectedRoute>
-          }
-        />
-        <Route 
-          path="/journal" 
-          element={
-            <ProtectedRoute>
-              <JournalPage/>
-            </ProtectedRoute>
-          }
-        />
-        <Route 
-          path="/profile" 
-          element={
-            <ProtectedRoute>
-              <ProfilePage/>
-            </ProtectedRoute>
-          }
-        />
-        <Route 
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminPage/>
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/village" element={ <ProtectedRoute><VillagePage /></ProtectedRoute>}/>
+        <Route path="/map" element={<ProtectedRoute><MapPage/></ProtectedRoute>}/>
+        <Route path="/inventory" element={<ProtectedRoute><InventoryPage/></ProtectedRoute>}/>
+        <Route path="/journal" element={<ProtectedRoute><JournalPage/></ProtectedRoute>}/>
+        <Route path="/quests" element={<ProtectedRoute><QuestsPage/></ProtectedRoute>}/>
+        <Route path="/profile" element={<ProtectedRoute><ProfilePage/></ProtectedRoute>}/>
+        <Route path="/admin" element={<ProtectedRoute><AdminPage/></ProtectedRoute>}/>
       </Route>
       <Route path="*" element={<Navigate to="/" replace />}/>
     </Routes>
